f12cb5afa6b8174bab957d5b8e881dfd05c17174
Moushami Reddy <moushami10@gmail.com>
2022-06-16 18:35:41 +0530

    Please enter the commit message for your changes. Lines starting

---
+++ javascript/assignment2/reverse.js
@@ -0,0 +1,13 @@
+
+function ReverseString(str) {
+	const revArray = [];
+	const length = str.length - 1;
+	
+	for(let i = length; i >= 0; i--) {
+		revArray.push(str[i]);
+	}
+	
+	return revArray.join('');
+}
+
+document.write(ReverseString("Hello"))
