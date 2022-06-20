3da5da7712e40a00bfd783e5ae7e67709e13cd9f
Moushami Reddy <moushami10@gmail.com>
2022-06-20 20:03:38 +0530

    assignment

--- javascript/assignment2/arrlength.js
+++ javascript/assignment2/arrlength.js
@@ -5,11 +5,11 @@ k[1]=6;
 k[2]=5;
 k[3]="undefined";
 k[9]=44;
-delete k[0];// actual length is 10
+delete k[0];
 console.log(k.length);
 k.shift();
-console.log(k.length);// length is 9
-for( var I in k){ // represents only elements defined
+console.log(k.length);
+for( var I in k){ 
     if(k.hasOwnProperty(I)){
         console.log(I);
     }
--- javascript/assignment3/filtereven.js
+++ javascript/assignment3/filtereven.js
@@ -2,7 +2,7 @@ function getEvenNumbers() {
     var arr = [4,5,7,8,14,45,76];
     
     var evens = arr.filter(number => number % 2 == 0);
-    document.write('Even Numbers: ' + evens);
+    console.log("Even Numbers: " + evens);
 }
 
 getEvenNumbers();
